c82d400bc92c772e3d6cff221bde0a7c61ae80e5
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2025-09-09 14:14:47 +0300

    Fix: prevent adding duplicate movies to Favorites and Watch lists

--- src/gui/favorites_panel.py
+++ src/gui/favorites_panel.py
@@ -4,6 +4,9 @@ from ..utils import save_movie_list, load_movie_list
 class FavoritesPanel(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        # Stored as self to access in other functions and avoid adding duplicate movies
+        self.favorites = None
+        self.to_watch = None
 
         self.fav_listbox_frame = ctk.CTkFrame(self, corner_radius=10)
         self.fav_listbox_frame.grid(row=0, column=0, sticky='n', pady=15, padx=15)
@@ -17,30 +20,37 @@ class FavoritesPanel(ctk.CTkFrame):
         self.refresh_list()
 
     def add_to_favorites(self, movie_data):
-        save_movie_list('favorites.json', movie_data)
-        self.refresh_list()
+        if any(movie['imdbID'] == movie_data['imdbID'] for movie in self.favorites):
+            return
+        else:
+            save_movie_list('favorites.json', movie_data)
+            self.refresh_list()
 
     def add_to_watch(self, movie_data):
-        save_movie_list('to_watch.json', movie_data)
-        self.refresh_list()
+        if any(movie['imdbID'] == movie_data['imdbID'] for movie in self.to_watch):
+            return
+        else:
+            save_movie_list('to_watch.json', movie_data)
+            self.refresh_list()
 
     def refresh_list(self):
         self.fav_listbox.configure(state='normal')
         self.watch_listbox.configure(state='normal')
         self.fav_listbox.delete('1.0', 'end')
         self.watch_listbox.delete('1.0', 'end')
-        favorites = load_movie_list('favorites.json')
-        to_watch = load_movie_list('to_watch.json')
+        # Stored as self to access in other functions and avoid adding duplicate movies
+        self.favorites = load_movie_list('favorites.json')
+        self.to_watch = load_movie_list('to_watch.json')
 
         # Dates are saved in CSV. Only titles are displayed for clarity.
         spaces = "═" * 16
         self.fav_listbox.insert('2.0', f"═{spaces} FAVORITES {spaces}\n")
-        for f in favorites:
+        for f in self.favorites:
             self.fav_listbox.insert('end', f"═ {f.get('Title')}\n")
         self.fav_listbox.configure(state='disabled')
 
         self.watch_listbox.insert('end', F'══{spaces} TO WATCH {spaces}\n')
-        for w in to_watch:
+        for w in self.to_watch:
             self.watch_listbox.insert('end', f"═ {w.get('Title')}\n")
         self.watch_listbox.configure(state='disabled')
 
